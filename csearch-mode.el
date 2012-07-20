02abf08de14e17afc9e3aeb7fc37a02985968fe3
Jason Filsinger <jason.filsinger@gmail.com>
2012-07-19 23:54:01 -0700

    major refactoring

--- csearch-mode.el
+++ csearch-mode.el
@@ -4,94 +4,90 @@
 ;;
 ;; author: Jason Filsinger
 ;;
-;;
-;; version 0.0.1
+;; version 0.1.0
 ;;
 ;; note: On OS X you might need to specify the path to the csearch executable.
 ;;       The osx GUI usually doesnt contain the propper search path
 ;;       for executable files (because it's not run from the shell).
 ;;
 ;;       e.g.
-;;       (setq csearch/csearch-executable "/usr/local/bin/csearch")
+;;       (setq csearch/program "/usr/local/bin/csearch")
 ;;
 
-(defvar csearch/result-regexp "^\\(.+?\\):\\([0-9]+?\\):\\(.+\\)[\n]")
-(defvar csearch/csearch-executable "csearch" "Path to the csearch executable")
+(defgroup csearch-mode nil
+  "*csearch"
+  :group 'csearch-mode)
 
-(defvar csearch/keymap (make-sparse-keymap))
-(define-key csearch/keymap (kbd "RET") 'csearch/select-match)
+(defcustom csearch/program "csearch"
+  "Path to the csearch executable"
+  :type `string
+  :group `csearch-mode)
 
-(defun csearch/cancel ()
-  ""
-  (kill-buffer "*csearch-list*")
-)
+(defcustom csearch/index-file nil
+  "codesearch index file.  This value is assigned to the environment variable CSEARCHINDEX before invoking csearch.  Default value is `~/.csearchindex'"
+  :type 'string
+  :group `csearch-mode)
 
-(defun csearch/get-result-match (pt &optional sub)
-  ""
-  (message "searching for string")
-  (let (ret-match)
-	(save-excursion
-	  (goto-char pt)
-	  (goto-char (point-at-bol))
+(defvar csearch/match-face 'match
+  "Face name to use for csearch matches")
 
-	  (when (re-search-forward csearch/result-regexp)
-		(if sub
-			(setq ret-match (match-string sub))
-		  (setq ret-match (match-string 0)))
-		)) ret-match))
+(defvar csearch/error-face 'compilation-error
+  "Face name to use for csearch matches")
 
+(defvar csearch/font-lock-keywords
+  `(("^.+\\(command not found\\): .+$" 0 csearch/error-face)
 
-(defun csearch/get-match-file-at-point (pt)
-  ""
-  (csearch/get-result-match pt 1))
-
-(defun csearch/get-match-string-at-point (pt)
-  ""
-  (csearch/get-result-match pt 3))
-
-(defun csearch/get-match-line-at-point (pt)
-  ""
-  (string-to-number (csearch/get-result-match pt 2)))
-
-(defun csearch/select-match ()
-  ""
-  (let ((selected-file (csearch/get-match-file-at-point (point) ))
-		(selected-line (csearch/get-match-line-at-point (point))) )
-	(if selected-file
-		(progn
-		  (find-file-existing selected-file)
-		  (goto-char (point-min))
-		  (forward-line selected-line))
-	  (error (concat "File not found: " selected-file)))))
+	("^.+open \\(.+\\):.?+\\(no such file or directory\\)" 0 csearch/error-face)
+	)
+  "Additional things to highlight in the csearch output.
+This gets tacked on the end of generated expressions.")
 
 (defun csearch/insert-sorted-lines (ARG &optional REVERSE)
   ""
   (save-excursion
 	(let ((entries-start (point)))
-	  (insert (shell-command-to-string (concat (if csearch/csearch-executable csearch/csearch-executable "csearch") " -n " regexp)))
+	  (insert ARG)
 	  (sort-lines nil entries-start (point)))))
 
-(defun csearch/csearch (regexp)
-  "Run the csearch tool"
+(defun csearch/csearch (regexp &optional case-insensitive index-file)
+  "Run the csearch tool and search for the provided REGEXP
+
+If CASE-INSENSITIVE is provided then csearch will perform a
+case-insensitive search.  If INDEX-FILE is provided then
+csearch will use the INDEX-FILE for it's search index.
+"
   (with-current-buffer (switch-to-buffer-other-window "*csearch-list*")
 	(setq buffer-read-only nil)
 	(erase-buffer)
-	(insert "Type RET on an entry to open file\n\n")
-	(insert (concat "Codesearch matches for regexp `" regexp "`:\n\n"))
+	(let ((csearch-command (concat
+							(if csearch/program csearch/program "csearch")
+							" "
+							(when case-insensitive "-i ")
+							"-n "
+							regexp))
+		  (prev-index-file (getenv "CSEARCHINDEX")))
+	  (setenv "CSEARCHINDEX" index-file)
+	  (insert "Type RET on an entry to open file\n\n")
+	  (insert (concat "" csearch-command "\n"))
+	  (insert (concat "Codesearch matches for regexp `" regexp "`:\n\n"))
+	  (csearch/insert-sorted-lines (shell-command-to-string csearch-command))
+	  (setenv "CSEARCHINDEX" prev-index-file))
 
-	(csearch/insert-sorted-lines (shell-command-to-string (concat "~/bin/csearch -n " regexp)))
-	(while (re-search-backward csearch/result-regexp nil t)
-	  (let ((curr-overlay (make-overlay (match-beginning 0) (match-end 0) nil nil t)))
-		(overlay-put curr-overlay 'priority 100)
-		(overlay-put curr-overlay 'keymap csearch/keymap)
-		(overlay-put curr-overlay 'evaporate t)
-		(overlay-put curr-overlay 'before-string " ")
-		))
+	(goto-char (point-min))
+	(forward-line 3)
+	(while (re-search-forward regexp nil t)
+	  (replace-match (propertize (match-string 0) 'face nil 'font-lock-face csearch/match-face)))
+
+	(goto-char (point-min))
+	(forward-line 5)
+	(font-lock-add-keywords 'grep-mode csearch/font-lock-keywords)
+	(grep-mode)
 	(setq buffer-read-only t)))
 
-(defun csearch/apropos (REGEXP)
+;;;###autoload
+(defun csearch/apropos (regexp)
   "Display a list of all symbols in the csearch index that REGEXP matches"
   (interactive "MSymbol (word or regexp): ")
-  (csearch/csearch REGEXP))
+  (csearch/csearch regexp nil csearch/index-file))
 
 (provide 'csearch-mode)
