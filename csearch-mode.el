e1b844ff5ace9ffbc507820913ddd079b4b38848
Jason Filsinger <jason.filsinger@gmail.com>
2012-10-02 00:09:01 -0700

    removing all tabs

--- csearch-mode.el
+++ csearch-mode.el
@@ -92,45 +92,45 @@
   "Set the current csearch index"
   (interactive "GIndex file: ")
   (let ((new-index-path (expand-file-name (if (file-directory-p index-path) (format "%s/.csearchindex" index-path) index-path))))
-	(unless (file-directory-p (file-name-directory index-path)) (error (format "Not a valid .csearchindex: %s" index-path)))
-	(setenv "CSEARCHINDEX" new-index-path) ))
+    (unless (file-directory-p (file-name-directory index-path)) (error (format "Not a valid .csearchindex: %s" index-path)))
+    (setenv "CSEARCHINDEX" new-index-path) ))
 
 ;;;###autoload
 (defun csearch/index-get ()
   "Get the current csearch index path"
   (or (getenv "CSEARCHINDEX")
-	  (if (file-exists-p (file-truename "~/.csearchindex"))
-		  (file-truename "~/.csearchindex")
-		(if (getenv "HOMEPATH")
-			(file-truename (format "%s/.csearchindex" (getenv "HOMEPATH")))
-		  nil))))
+      (if (file-exists-p (file-truename "~/.csearchindex"))
+          (file-truename "~/.csearchindex")
+        (if (getenv "HOMEPATH")
+            (file-truename (format "%s/.csearchindex" (getenv "HOMEPATH")))
+          nil))))
 
 ;;;###autoload
 (defmacro csearch/with-index-file (index-file &rest body)
   "Execute the forms in BODY with the index-file temporarly set as the current csearch index."
   (declare (indent 1) (debug t))
   `(let ((prev-index-file (csearch/index-get)))
-	  (progn (when ,index-file (csearch/index-set (file-truename ,index-file)))
-			 ,@body
-			 (csearch/index-set prev-index-file))))
+      (progn (when ,index-file (csearch/index-set (file-truename ,index-file)))
+             ,@body
+             (csearch/index-set prev-index-file))))
 
 ;;;###autoload
 (defun csearch/find-file-upward (file &optional startdir)
   "Search the directory tree upwards for a specific file"
   (let ((dirname (expand-file-name (if startdir startdir (file-name-directory (buffer-file-name)))))
-		(found nil))
-	(while (not (or found (string= dirname "/")))
-	  (setq found (if (file-exists-p (format "%s%s" dirname file )) dirname nil))
-	  (setq dirname (expand-file-name "../" dirname)) )
-	(expand-file-name file found) ))
+        (found nil))
+    (while (not (or found (string= dirname "/")))
+      (setq found (if (file-exists-p (format "%s%s" dirname file )) dirname nil))
+      (setq dirname (expand-file-name "../" dirname)) )
+    (expand-file-name file found) ))
 
 ;;;###autoload
 (defun csearch/insert-sorted-lines (ARG &optional REVERSE)
   ""
   (save-excursion
-	(let ((entries-start (point)))
-	  (insert ARG)
-	  (sort-lines nil entries-start (point)))))
+    (let ((entries-start (point)))
+      (insert ARG)
+      (sort-lines nil entries-start (point)))))
 
 ;;;###autoload
 (defun csearch/filter ()
@@ -156,12 +156,12 @@ This function is called from `compilation-filter-hook'."
         (goto-char beg)
         (while (re-search-forward "\033\\[[0-9;]*[mK]" end 1)
           (replace-match "" t t))
-		;; offset the line numbers by 1 (csearch uses the 0 as the first line number, emacs uses 1
-		(when csearch/result-line-offset
-		  (goto-char beg)
-		  (while (re-search-forward csearch/result-regexp end 1)
-			(replace-match (number-to-string (+ (string-to-number (match-string 2)) csearch/result-line-offset)) t t nil 2 ) )
-		  )))))
+        ;; offset the line numbers by 1 (csearch uses the 0 as the first line number, emacs uses 1
+        (when csearch/result-line-offset
+          (goto-char beg)
+          (while (re-search-forward csearch/result-regexp end 1)
+            (replace-match (number-to-string (+ (string-to-number (match-string 2)) csearch/result-line-offset)) t t nil 2 ) )
+          )))))
 
 ;;;###autoload
 (define-compilation-mode csearch-mode "csearch"
@@ -195,42 +195,42 @@ csearch will use the INDEX-FILE for it's search index.
   "Delete all of the current csearch index"
   (interactive)
   (csearch/with-index-file index-file
-	(shell-command-to-string (format "%s --reset" (if csearch/cindex-program csearch/cindex-program "cindex")))))
+    (shell-command-to-string (format "%s --reset" (if csearch/cindex-program csearch/cindex-program "cindex")))))
 
 ;;;###autoload
 (defun csearch/index-add (path &optional index-file synchronous)
   "Add a path to the csearch index"
   (interactive "GPath:")
   (with-temp-message "generating cindex..."
-	(let ((cindex-command (format "%s %s %s"
-								  (shell-quote-argument (if csearch/cindex-program csearch/cindex-program "cindex"))
-								  (expand-file-name path)
-								  (if synchronous "" "&") )))
-	  (csearch/with-index-file index-file (shell-command cindex-command "*cindex*") ) )))
+    (let ((cindex-command (format "%s %s %s"
+                                  (shell-quote-argument (if csearch/cindex-program csearch/cindex-program "cindex"))
+                                  (expand-file-name path)
+                                  (if synchronous "" "&") )))
+      (csearch/with-index-file index-file (shell-command cindex-command "*cindex*") ) )))
 
 ;;;###autoload
 (defun csearch/index-list (&optional index-file)
   "Returns a list of paths that were used to generate the current cindex"
   (let (cindex-list)
-	(with-temp-buffer
-	  (csearch/with-index-file index-file
-		(shell-command (format "%s --list" (shell-quote-argument (if csearch/cindex-program csearch/cindex-program "cindex"))) (current-buffer)))
-	  (save-excursion
-		(goto-char (point-min))
-		(while (re-search-forward "^\\([c-z/]+?:*[a-z/\\]+\\)$" nil t)
-		  (add-to-list 'cindex-list (match-string 1))))
-	  ) cindex-list) )
+    (with-temp-buffer
+      (csearch/with-index-file index-file
+        (shell-command (format "%s --list" (shell-quote-argument (if csearch/cindex-program csearch/cindex-program "cindex"))) (current-buffer)))
+      (save-excursion
+        (goto-char (point-min))
+        (while (re-search-forward "^\\([c-z/]+?:*[a-z/\\]+\\)$" nil t)
+          (add-to-list 'cindex-list (match-string 1))))
+      ) cindex-list) )
 
 ;;;###autoload
 (defun csearch/index-add-list (file-list &optional index-file)
   "Add a list of paths to the csearch index"
   (let ((index-path ""))
-	(loop for path in file-list do (setq index-path (format "%s %s" index-path (shell-quote-argument path))) )
-	(csearch/with-index-file index-file
-	  (shell-command (format "%s --reset%s &"
-							 (shell-quote-argument (if csearch/cindex-program csearch/cindex-program "cindex"))
-							 index-path)
-					 "*cindex*") )))
+    (loop for path in file-list do (setq index-path (format "%s %s" index-path (shell-quote-argument path))) )
+    (csearch/with-index-file index-file
+      (shell-command (format "%s --reset%s &"
+                             (shell-quote-argument (if csearch/cindex-program csearch/cindex-program "cindex"))
+                             index-path)
+                     "*cindex*") )))
 
 ;;;###autoload
 (defun csearch/index-regenerate (&optional index-file)
@@ -242,9 +242,9 @@ csearch will use the INDEX-FILE for it's search index.
 (defun csearch/read-string-region-or-prompt-string (prompt &optional history non-interactive)
   "Similar to read-string except it sets the default value to the selected region or the thing-at-point ('symbol)"
   (let ((default-search-string (if (region-active-p) (buffer-substring-no-properties (region-beginning) (region-end)) (thing-at-point 'symbol) )))
-		  (if non-interactive
-			  (substring-no-properties default-search-string)
-			  (read-string (format "%s%s: " prompt (if default-search-string (format " [default: %s]" default-search-string) "") ) nil history default-search-string) )))
+          (if non-interactive
+              (substring-no-properties default-search-string)
+              (read-string (format "%s%s: " prompt (if default-search-string (format " [default: %s]" default-search-string) "") ) nil history default-search-string) )))
 
 ;;;###autoload
 (defun csearch/search (regexp)
@@ -257,6 +257,6 @@ csearch will use the INDEX-FILE for it's search index.
   "This function can be called from the find-file-hook to automatically
 seach the directory tree upwards for a \".csearchindex\" file."
   (let ((tree-index (csearch/find-file-upward ".csearchindex")))
-	(set (make-local-variable 'csearch/index-file) tree-index)))
+    (set (make-local-variable 'csearch/index-file) tree-index)))
 
 (provide 'csearch-mode)
